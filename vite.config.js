0680b97cff1886a66e92c70678aa313815429e35
renaldi-oss <reynaldifp2000@gmail.com>
2022-12-28 00:32:50 +0700

    perubahan config vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import laravel,{ refreshPaths } from 'laravel-vite-plugin'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/sass/app.scss', 'resources/js/app.js'],
+            input: ['resources/sass/app.scss', 'resources/js/app.js','resources/css/app.css'],
             refresh : [
                 'app/Http/Livewire/**',
                 'app/Forms/Components/**',
