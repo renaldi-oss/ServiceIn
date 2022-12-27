f7af10352beaf729092b63e65332faa0d83d4198
renaldi-oss <reynaldifp2000@gmail.com>
2022-12-28 00:58:21 +0700

    coba lage

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             // input: ['resources/sass/app.scss', 'resources/js/app.js'],
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js','resources/sass/app.scss'],
             refresh : [
                 'app/Http/Livewire/**',
                 'app/Forms/Components/**',
