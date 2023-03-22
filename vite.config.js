72456c1caeb7ae13ec9a0c8b8f51bd2b5b189eec
renaldi-oss <reynaldifp2000@gmail.com>
2023-03-22 22:45:01 +0700

    deploy heroku

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import laravel,{ refreshPaths } from 'laravel-vite-plugin'
 export default defineConfig({
     plugins: [
         laravel({
-            // input: ['resources/sass/app.scss', 'resources/js/app.js'],
-            input: ['resources/css/app.css', 'resources/js/app.js','resources/sass/app.scss'],
+            input: ['resources/sass/app.scss', 'resources/js/app.js'],
+            // input: ['resources/css/app.css', 'resources/js/app.js','resources/sass/app.scss'],
             refresh : [
                 'app/Http/Livewire/**',
                 'app/Forms/Components/**',
